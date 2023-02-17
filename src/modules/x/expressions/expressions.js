63c65a7a994b8e8d49348dd6cd388172be186f31
leobalter <leonardo.balter@gmail.com>
2023-02-16 17:03:49 -0800

    3 files modified

--- src/modules/x/expressions/expressions.js
+++ src/modules/x/expressions/expressions.js
@@ -2,8 +2,4 @@ import { LightningElement } from 'lwc';
 
 export default class extends LightningElement {
   opRange = 10;
-
-  opRangeChange({ target: { value } }) {
-    this.opRange = value;
-  }
 }
